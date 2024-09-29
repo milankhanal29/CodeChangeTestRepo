bd92406b31b37812ba4e5ae09e24e6613fc0cea1
milankhanal29 <meelonkhanal2@gmail.com>
2024-09-29 11:02:43 +0545

    changes

--- Order.java
+++ Order.java
@@ -4,8 +4,5 @@ public class Order {
     private int id;
     private int orderId;
     private int productId;
-    private int pid;
-    private Date expiryDate;
-    private Date createdDate;
 }
 
